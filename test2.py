e47e2e93b1e44f3b86fcd836ac190238a47b5941
Sepideh Azarnoosh <azarnoos@usc.edu>
2016-05-26 13:48:04 -0700

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,12 +1,14 @@
 from WordLevel import WordLevel
 #from TopicModelingPreprocessing import TopicModelingPreprocessing
 from TopicModeling import TopicModeling
+from SentimentAnalysis import SentimentAnalysis
 
 text = '"A\\xc2\\xa0majority of Canadians can and has been wrong before now and will be again.\\n\\nUnless you\'re fucking supportive of the idea. "' 
 #that nothing is full proof or perfect so you take your chances and if we should inadvertently kill your son or daughter then them\'s the breaks and we can always regard you as collateral damage like in wartime - and sorry, but\\xc2\\xa0the cheques in the mail. "'
 myWordLevel = WordLevel(text, 'upperCount, quoteRemove, decoding, repetitionCount, repetitionRemove, spellCorrector, cleaning, wordCount, charCount, negWordCount, posWordCount, badWordCount')
 
 myTopicModeling = TopicModeling()
+mySentimentAnalysis = SentimentAnalysis()
 #myTopicModelingPreprocessing = TopicModelingPreprocessing()
 # import lda
 # X = lda.datasets.load_reuters()
